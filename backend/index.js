54b7c5b72941de8414c75119691265d4238e5d17
SoumyadeepOSD <opencontainer255@gmail.com>
2024-04-07 08:30:37 +0530

    changes done in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const path = require('path');
 
 app.use(express.json());
 app.use(cors(
@@ -18,20 +17,9 @@ dbConnect();
 
 app.use('/auth', userRoutes);
 
-// -------------DEPLOYMENT-------------
-// const __dirname1 = path.resolve();
-// if(process.env.NODE_ENV==='production'){
-//     app.use(express.static(path.join(__dirname1,"/frontend/build")));
-
-//     app.get("*", (req, res)=>{
-//         res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-//     })
-// }else{
-//     app.get("/", (req, res) => {
-//         console.log("Hello World");
-//     });    
-// }
-app.get("/", (req, res)=>console.log("Hello World"));
+app.get("/", (req, res)=>{
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
